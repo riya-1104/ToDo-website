d666819db819fea60ed774aee03bf25cfe1cf169
Riya <riyakarn1107@gmail.com>
2026-09-10 20:20:34 +0530

    Complete CRUD functionality

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
-    return render(request , 'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
+    
+    context = {
+        'tasks':tasks,
+        'completed_tasks':completed_tasks
+    }
+    return render(request ,'home.html',context)
